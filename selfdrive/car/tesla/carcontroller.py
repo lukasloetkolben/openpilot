8532c208c72761350fc58c2b8053a53f1e690b45
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2025-05-19 21:15:45 +0200

    fix

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -37,7 +37,7 @@ class CarController(CarControllerBase):
 
     # Longitudinal control
     if self.frame % 4 == 0:
-      state = 13 if (CC.cruiseControl.cancel or CS.steering_disengage) else 4  # 4=ACC_ON, 13=ACC_CANCEL_GENERIC_SILENT
+      state = CS.das_control["DAS_accState"]
       accel = float(np.clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX))
       cntr = (self.frame // 4) % 8
       can_sends.append(self.tesla_can.create_longitudinal_command(state, accel, cntr, CS.out.vEgo, CC.longActive))
