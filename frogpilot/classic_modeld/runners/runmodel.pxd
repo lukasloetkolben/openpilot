nkaoud-fp/no_comp

FrogPilot 0.9.7

selfdrive/classic_modeld/runners/runmodel.pxd
@@ -1,0 +1,14 @@
+# distutils: language = c++
+
+from libcpp.string cimport string
+
+cdef extern from "selfdrive/classic_modeld/runners/runmodel.h":
+  cdef int USE_CPU_RUNTIME
+  cdef int USE_GPU_RUNTIME
+  cdef int USE_DSP_RUNTIME
+
+  cdef cppclass RunModel:
+    void addInput(string, float*, int)
+    void setInputBuffer(string, float*, int)
+    void * getCLBuffer(string)
+    void execute()

tinygrad_repo/extra/assembly/assembly_arm64.py
@@ -1,9 +1,10 @@
 import struct
 from platform import system
 from typing import Tuple, Dict, List, Optional
+from tinygrad import dtypes
 from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
-from tinygrad.codegen.linearizer import UOps, UOp
-from tinygrad.helpers import dtypes, CI
+from tinygrad.codegen.kernel import Ops, UOp
+from tinygrad.helpers import CI
 from tinygrad.codegen.assembly import uops_to_asmstyle, AssemblyLanguage
 
 def float_to_hex(x): return "%02X%02X%02X%02X" % tuple(struct.pack("f",x)[::-1])
@@ -18,7 +19,7 @@ class ARM64Language(AssemblyLanguage): pass
 
 def specialize_to_arm64(fn_nm, asm):
   var_size = 16
-  prev_uop:Optional[UOps] = None
+  prev_uop:Optional[Ops] = None
   ins = []
   x_regs = ['x' + str(i) for i in reversed(range(12))]
   s_regs = ['s' + str(i) for i in reversed(range(3,32)) if i <= 7 or i >= 16]
@@ -80,7 +81,7 @@ def specialize_to_arm64(fn_nm, asm):
       ins.append(f"mov x15, {mem_vars[v.nm]}")
       ins.append(f"ldr {rtor[v.nm]}, [sp, x15]")
 
-    if uop == UOps.SPECIAL:
+    if uop == Ops.SPECIAL:
       if arg.startswith('data'):
         # data 8 to n into the stack
         if int(arg[4:]) >= 8:
@@ -89,7 +90,7 @@ def specialize_to_arm64(fn_nm, asm):
       else:
         ins.append(f"mov {rtor[out.nm]}, #0")
         ins.append(f"loop_{arg}:")
-    elif uop == UOps.CAST:
+    elif uop == Ops.CAST:
       if arg == BinaryOps.CMPLT:
         if rtor[out.nm][0] == 's':
           mov_imm(0.0, 's0')
@@ -101,7 +102,7 @@ def specialize_to_arm64(fn_nm, asm):
           ins.append(f"csel {rtor[out.nm]}, x15, x14, lt")
       else:
         ins.append(f"sxtw {rtor[out.nm]}, w{rtor[vin[0].nm][1:]}")
-    elif uop == UOps.ALU:
+    elif uop == Ops.ALU:
       if len(vin)==2 and vin[1].__class__ is int: mov_imm(vin[1], 'x15')
       if arg == BinaryOps.MUL and out.dtype == dtypes.bool:
         ins.append(f"ands {','.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
@@ -135,7 +136,7 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"msub {rtor[out.nm]}, x14, {rhs}, {rtor[vin[0].nm]}")
       else:
         ins.append(f"{'f' if dtypes.is_float(vin[0][1]) else 's' if arg == BinaryOps.DIV else ''}{alu[arg]} {', '.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
-    elif uop == UOps.LOAD:
+    elif uop == Ops.LOAD:
       if arg.__class__ in (int, float):
         mov_imm(arg, rtor[out.nm])
       else:
@@ -145,20 +146,20 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"add x15, {rtor[vin[0].nm]}, x15")
         ins.append(f"ldr{'sb' if arg[2] is not None and arg[2] in (dtypes.int8, dtypes.uint8, dtypes.bool) else ''} {reg_in}, [x15]")
         if arg[2] is not None: ins.append(f"{'fcvt' if arg[2] in [dtypes.half, dtypes.double] else 'scvtf'} {rtor[out.nm]}, {reg_in}")
-    elif uop == UOps.STORE:
+    elif uop == Ops.STORE:
       #NOTE: if need casting load var in s/h0 or x/w12 temp regs
       reg_out = (type_to_reg[arg[2]] + ('0' if dtypes.is_float(arg[2]) else '12') if arg[2] is not None else rtor[vin[1].nm])
       if arg[2] is not None: ins.append(f"fcvt{'zs' if arg[2] not in [dtypes.half, dtypes.double] else '' } {reg_out}, {rtor[vin[1].nm]}")
       ins.append(f"mov x15, #{arg[0]}")
       ins.append(f"str {reg_out}, [{rtor[vin[0].nm]}, x15, lsl #0]")
-    elif uop == UOps.COND_BRANCH:
+    elif uop == Ops.COND_BRANCH:
       #TODO: this is a hack it shouldn't always be a cmp before a cond branch?
-      if prev_uop == UOps.LOAD:
+      if prev_uop == Ops.LOAD:
         ins.append(f"cmp {rtor[vin[0].nm]}, #0")
       ins.append(f"b.{'lt' if arg[1] else 'ge'} {arg[0][1:]}")
-    elif uop == UOps.LABEL:
+    elif uop == Ops.LABEL:
       ins.append(f"{arg[1:]}:")
-    elif uop == UOps.ENDLOOP:
+    elif uop == Ops.ENDLOOP:
       mov_imm(arg[0], "x15")
       ins.append(f"add {rtor[vin[0].nm]}, {rtor[vin[0].nm]}, #1")
       ins.append(f"cmp {rtor[vin[0].nm]}, x15")

tinygrad_repo/extra/assembly/assembly_rdna.py
@@ -1,8 +1,8 @@
 import yaml
 from typing import Tuple, Set, Dict
-from tinygrad.helpers import dtypes
+from tinygrad import dtypes
 from tinygrad.codegen.assembly import AssemblyCodegen, Register
-from tinygrad.codegen.linearizer import UOps
+from tinygrad.codegen.kernel import Ops
 from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
 from tinygrad.runtime.ops_gpu import ROCM_LLVM_PATH
 
@@ -61,8 +61,8 @@ class RDNACodegen(AssemblyCodegen):
     def reg_out(x):
       return rtor[x]
     for uop, out, vin, arg in asm:
-      if uop == UOps.DEFINE_REGISTER:
-        if arg[0][0] in [dtypes.uint32, dtypes.uint64, dtypes.int64, dtypes.int32, dtypes.float32, dtypes._float4]:
+      if uop == Ops.DEFINE_REGISTER:
+        if arg[0][0] in [dtypes.uint32, dtypes.uint64, dtypes.int64, dtypes.int32, dtypes.float32, dtypes.float.vec(4)]:
           for i in range(arg[2]):
             # TODO: Re-use gaps created by this to avoid wasting registers
             align = int(arg[0][0].itemsize / 4)
@@ -76,7 +76,7 @@ class RDNACodegen(AssemblyCodegen):
               v_cnt += align
             rtor[Register(f"%{arg[1]}{i}", *arg[0])] = reg_name
 
-            if arg[0][0] == dtypes._float4:
+            if arg[0][0] == dtypes.float.vec(4):
               for off in range(4):
                 reg_name = f"s{s_cnt-align+off}" if arg[0][1] else f"v{v_cnt-align+off}"
                 rtor[Register(f"%{arg[1]}{i}", dtypes.float, False, off=off)] = reg_name
@@ -86,7 +86,7 @@ class RDNACodegen(AssemblyCodegen):
             rtor[Register(f"%{arg[1]}{i}", *arg[0])] = reg_name
         else:
           raise NotImplementedError("DEFINE_REGISTER not implemented for arg: ", arg)
-      elif uop == UOps.SPECIAL:
+      elif uop == Ops.SPECIAL:
         if arg.startswith('buf'):
           i = int(arg[3:])
           ins.append(f's_load_b64 {reg_out(out)}, s[0:1], {i*8}')
@@ -106,15 +106,15 @@ class RDNACodegen(AssemblyCodegen):
           pend_regs.clear()
           ins.append(f'v_mul_i32_i24 {reg_out(out)}, {reg_out(out)}, s{2+int(arg[3])}')
           ins.append(f'v_add_nc_u32 {reg_out(out)}, v{int(arg[3])}, {reg_out(out)}')
-      elif uop == UOps.CONST:
+      elif uop == Ops.CONST:
         if arg == float('inf'): arg = "0x7f800000"
         elif arg == float('-inf'): arg = "0xff800000"
-        if out.dtype == dtypes._float4:
+        if out.dtype == dtypes.float.vec(4):
           for off in range(4):
             ins.append(f"{'s_' if out.scalar else 'v_'}mov_b32 {reg_out(Register(out.nm, dtypes.float, False, off=off))}, {arg}")
         else:
           ins.append(f"{'s_' if out.scalar else 'v_'}mov_b32 {reg_out(out)}, {arg}")
-      elif uop == UOps.ALU:
+      elif uop == Ops.ALU:
         if arg in [BinaryOps.CMPLT]:
           ins.append(f"{'s' if out.scalar else 'v'}_{alu[arg]}_{dtype_to_rdnatype[out.dtype]} {', '.join(reg_in(x) if x.__class__ is Register else str(x) for x in vin)}")
         else:
@@ -122,26 +122,26 @@ class RDNACodegen(AssemblyCodegen):
           if arg == TernaryOps.MULACC and out == vin[2]:
             alu_arg = "fmac"
             vin = vin[0:2]
-          if out.dtype == dtypes._float4:
-            for rr in zip(*[x.subregs() if x.dtype == dtypes._float4 else [x,x,x,x] for x in [out]+vin]):
+          if out.dtype == dtypes.float.vec(4):
+            for rr in zip(*[x.subregs() if x.dtype == dtypes.float.vec(4) else [x,x,x,x] for x in [out]+vin]):
               ins.append(f"{'s_' if rr[0].scalar else 'v_'}{alu_arg}_{dtype_to_rdnatype[rr[0].dtype]} {reg_out(rr[0])}, {', '.join(reg_in(x) if x.__class__ is Register else str(x) for x in rr[1:])}")
           else:
             ins.append(f"{'s_' if out.scalar else 'v_'}{alu_arg}_{dtype_to_rdnatype[out.dtype] if arg != UnaryOps.NOOP else 'b32'}{'_i24' if arg == BinaryOps.MUL and out.dtype != dtypes.float32 and not out.scalar else ''} {reg_out(out)}, {', '.join(reg_in(x) if x.__class__ is Register else str(x) for x in vin)}")
-      elif uop == UOps.LOAD:
+      elif uop == Ops.LOAD:
         if out.scalar:
           # swap arg order
           ins.append(f's_load_b32 {reg_out(out)}, {reg_in(vin[0])}, {reg_in(vin[1])} offset:{arg[0]}')
         else:
-          ins.append(f'global_load_{"b128" if out.dtype == dtypes._float4 else "b32"} {reg_out(out)}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
+          ins.append(f'global_load_{"b128" if out.dtype == dtypes.float.vec(4) else "b32"} {reg_out(out)}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
         pend_regs.add(out)
         for r in out.subregs(): pend_regs.add(r)
-      elif uop == UOps.STORE:
-        ins.append(f'global_store_{"b128" if vin[1].dtype == dtypes._float4 else "b32"} {reg_in(vin[2])}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
-      elif uop == UOps.LABEL:
+      elif uop == Ops.STORE:
+        ins.append(f'global_store_{"b128" if vin[1].dtype == dtypes.float.vec(4) else "b32"} {reg_in(vin[2])}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
+      elif uop == Ops.LABEL:
         ins.append(f"{arg}:")
-      elif uop == UOps.COND_BRANCH:
+      elif uop == Ops.COND_BRANCH:
         ins.append(f"s_cbranch_scc{'1' if arg[1] else '0'} {arg[0]}")
-      elif uop == UOps.CAST:
+      elif uop == Ops.CAST:
         if vin[0].dtype == dtypes.bool:
           if out.dtype == dtypes.float32:
             ins.append(f"v_cndmask_b32 {reg_out(out)}, 0.0, 1.0, {reg_in(vin[0])}")

lukasloetkolben/openpilot

frogpilot/classic_modeld/runners/runmodel.pxd
@@ -1,0 +1,14 @@
+# distutils: language = c++
+
+from libcpp.string cimport string
+
+cdef extern from "frogpilot/classic_modeld/runners/runmodel.h":
+  cdef int USE_CPU_RUNTIME
+  cdef int USE_GPU_RUNTIME
+  cdef int USE_DSP_RUNTIME
+
+  cdef cppclass RunModel:
+    void addInput(string, float*, int)
+    void setInputBuffer(string, float*, int)
+    void * getCLBuffer(string)
+    void execute()

tinygrad_repo/extra/assembly/assembly_arm64.py
@@ -1,9 +1,10 @@
 import struct
 from platform import system
 from typing import Tuple, Dict, List, Optional
-from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
-from tinygrad.codegen.linearizer import UOps, UOp
-from tinygrad.helpers import dtypes, CI
+from tinygrad import dtypes
+from tinygrad.uop.ops import BinaryOps, UnaryOps, TernaryOps
+from tinygrad.opt.kernel import Ops, UOp
+from tinygrad.helpers import CI
 from tinygrad.codegen.assembly import uops_to_asmstyle, AssemblyLanguage
 
 def float_to_hex(x): return "%02X%02X%02X%02X" % tuple(struct.pack("f",x)[::-1])
@@ -18,7 +19,7 @@ class ARM64Language(AssemblyLanguage): pass
 
 def specialize_to_arm64(fn_nm, asm):
   var_size = 16
-  prev_uop:Optional[UOps] = None
+  prev_uop:Optional[Ops] = None
   ins = []
   x_regs = ['x' + str(i) for i in reversed(range(12))]
   s_regs = ['s' + str(i) for i in reversed(range(3,32)) if i <= 7 or i >= 16]
@@ -80,7 +81,7 @@ def specialize_to_arm64(fn_nm, asm):
       ins.append(f"mov x15, {mem_vars[v.nm]}")
       ins.append(f"ldr {rtor[v.nm]}, [sp, x15]")
 
-    if uop == UOps.SPECIAL:
+    if uop == Ops.SPECIAL:
       if arg.startswith('data'):
         # data 8 to n into the stack
         if int(arg[4:]) >= 8:
@@ -89,7 +90,7 @@ def specialize_to_arm64(fn_nm, asm):
       else:
         ins.append(f"mov {rtor[out.nm]}, #0")
         ins.append(f"loop_{arg}:")
-    elif uop == UOps.CAST:
+    elif uop == Ops.CAST:
       if arg == BinaryOps.CMPLT:
         if rtor[out.nm][0] == 's':
           mov_imm(0.0, 's0')
@@ -101,7 +102,7 @@ def specialize_to_arm64(fn_nm, asm):
           ins.append(f"csel {rtor[out.nm]}, x15, x14, lt")
       else:
         ins.append(f"sxtw {rtor[out.nm]}, w{rtor[vin[0].nm][1:]}")
-    elif uop == UOps.ALU:
+    elif uop == Ops.ALU:
       if len(vin)==2 and vin[1].__class__ is int: mov_imm(vin[1], 'x15')
       if arg == BinaryOps.MUL and out.dtype == dtypes.bool:
         ins.append(f"ands {','.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
@@ -135,7 +136,7 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"msub {rtor[out.nm]}, x14, {rhs}, {rtor[vin[0].nm]}")
       else:
         ins.append(f"{'f' if dtypes.is_float(vin[0][1]) else 's' if arg == BinaryOps.DIV else ''}{alu[arg]} {', '.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
-    elif uop == UOps.LOAD:
+    elif uop == Ops.LOAD:
       if arg.__class__ in (int, float):
         mov_imm(arg, rtor[out.nm])
       else:
@@ -145,20 +146,20 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"add x15, {rtor[vin[0].nm]}, x15")
         ins.append(f"ldr{'sb' if arg[2] is not None and arg[2] in (dtypes.int8, dtypes.uint8, dtypes.bool) else ''} {reg_in}, [x15]")
         if arg[2] is not None: ins.append(f"{'fcvt' if arg[2] in [dtypes.half, dtypes.double] else 'scvtf'} {rtor[out.nm]}, {reg_in}")
-    elif uop == UOps.STORE:
+    elif uop == Ops.STORE:
       #NOTE: if need casting load var in s/h0 or x/w12 temp regs
       reg_out = (type_to_reg[arg[2]] + ('0' if dtypes.is_float(arg[2]) else '12') if arg[2] is not None else rtor[vin[1].nm])
       if arg[2] is not None: ins.append(f"fcvt{'zs' if arg[2] not in [dtypes.half, dtypes.double] else '' } {reg_out}, {rtor[vin[1].nm]}")
       ins.append(f"mov x15, #{arg[0]}")
       ins.append(f"str {reg_out}, [{rtor[vin[0].nm]}, x15, lsl #0]")
-    elif uop == UOps.COND_BRANCH:
+    elif uop == Ops.COND_BRANCH:
       #TODO: this is a hack it shouldn't always be a cmp before a cond branch?
-      if prev_uop == UOps.LOAD:
+      if prev_uop == Ops.LOAD:
         ins.append(f"cmp {rtor[vin[0].nm]}, #0")
       ins.append(f"b.{'lt' if arg[1] else 'ge'} {arg[0][1:]}")
-    elif uop == UOps.LABEL:
+    elif uop == Ops.LABEL:
       ins.append(f"{arg[1:]}:")
-    elif uop == UOps.ENDLOOP:
+    elif uop == Ops.ENDLOOP:
       mov_imm(arg[0], "x15")
       ins.append(f"add {rtor[vin[0].nm]}, {rtor[vin[0].nm]}, #1")
       ins.append(f"cmp {rtor[vin[0].nm]}, x15")

tinygrad_repo/extra/assembly/assembly_rdna.py
@@ -1,9 +1,9 @@
 import yaml
 from typing import Tuple, Set, Dict
-from tinygrad.helpers import dtypes
+from tinygrad import dtypes
 from tinygrad.codegen.assembly import AssemblyCodegen, Register
-from tinygrad.codegen.linearizer import UOps
-from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
+from tinygrad.opt.kernel import Ops
+from tinygrad.uop.ops import BinaryOps, UnaryOps, TernaryOps
 from tinygrad.runtime.ops_gpu import ROCM_LLVM_PATH
 
 # ugh, is this really needed?
@@ -61,8 +61,8 @@ class RDNACodegen(AssemblyCodegen):
     def reg_out(x):
       return rtor[x]
     for uop, out, vin, arg in asm:
-      if uop == UOps.DEFINE_REGISTER:
-        if arg[0][0] in [dtypes.uint32, dtypes.uint64, dtypes.int64, dtypes.int32, dtypes.float32, dtypes._float4]:
+      if uop == Ops.DEFINE_REGISTER:
+        if arg[0][0] in [dtypes.uint32, dtypes.uint64, dtypes.int64, dtypes.int32, dtypes.float32, dtypes.float.vec(4)]:
           for i in range(arg[2]):
             # TODO: Re-use gaps created by this to avoid wasting registers
             align = int(arg[0][0].itemsize / 4)
@@ -76,7 +76,7 @@ class RDNACodegen(AssemblyCodegen):
               v_cnt += align
             rtor[Register(f"%{arg[1]}{i}", *arg[0])] = reg_name
 
-            if arg[0][0] == dtypes._float4:
+            if arg[0][0] == dtypes.float.vec(4):
               for off in range(4):
                 reg_name = f"s{s_cnt-align+off}" if arg[0][1] else f"v{v_cnt-align+off}"
                 rtor[Register(f"%{arg[1]}{i}", dtypes.float, False, off=off)] = reg_name
@@ -86,7 +86,7 @@ class RDNACodegen(AssemblyCodegen):
             rtor[Register(f"%{arg[1]}{i}", *arg[0])] = reg_name
         else:
           raise NotImplementedError("DEFINE_REGISTER not implemented for arg: ", arg)
-      elif uop == UOps.SPECIAL:
+      elif uop == Ops.SPECIAL:
         if arg.startswith('buf'):
           i = int(arg[3:])
           ins.append(f's_load_b64 {reg_out(out)}, s[0:1], {i*8}')
@@ -106,15 +106,15 @@ class RDNACodegen(AssemblyCodegen):
           pend_regs.clear()
           ins.append(f'v_mul_i32_i24 {reg_out(out)}, {reg_out(out)}, s{2+int(arg[3])}')
           ins.append(f'v_add_nc_u32 {reg_out(out)}, v{int(arg[3])}, {reg_out(out)}')
-      elif uop == UOps.CONST:
+      elif uop == Ops.CONST:
         if arg == float('inf'): arg = "0x7f800000"
         elif arg == float('-inf'): arg = "0xff800000"
-        if out.dtype == dtypes._float4:
+        if out.dtype == dtypes.float.vec(4):
           for off in range(4):
             ins.append(f"{'s_' if out.scalar else 'v_'}mov_b32 {reg_out(Register(out.nm, dtypes.float, False, off=off))}, {arg}")
         else:
           ins.append(f"{'s_' if out.scalar else 'v_'}mov_b32 {reg_out(out)}, {arg}")
-      elif uop == UOps.ALU:
+      elif uop == Ops.ALU:
         if arg in [BinaryOps.CMPLT]:
           ins.append(f"{'s' if out.scalar else 'v'}_{alu[arg]}_{dtype_to_rdnatype[out.dtype]} {', '.join(reg_in(x) if x.__class__ is Register else str(x) for x in vin)}")
         else:
@@ -122,26 +122,26 @@ class RDNACodegen(AssemblyCodegen):
           if arg == TernaryOps.MULACC and out == vin[2]:
             alu_arg = "fmac"
             vin = vin[0:2]
-          if out.dtype == dtypes._float4:
-            for rr in zip(*[x.subregs() if x.dtype == dtypes._float4 else [x,x,x,x] for x in [out]+vin]):
+          if out.dtype == dtypes.float.vec(4):
+            for rr in zip(*[x.subregs() if x.dtype == dtypes.float.vec(4) else [x,x,x,x] for x in [out]+vin]):
               ins.append(f"{'s_' if rr[0].scalar else 'v_'}{alu_arg}_{dtype_to_rdnatype[rr[0].dtype]} {reg_out(rr[0])}, {', '.join(reg_in(x) if x.__class__ is Register else str(x) for x in rr[1:])}")
           else:
             ins.append(f"{'s_' if out.scalar else 'v_'}{alu_arg}_{dtype_to_rdnatype[out.dtype] if arg != UnaryOps.NOOP else 'b32'}{'_i24' if arg == BinaryOps.MUL and out.dtype != dtypes.float32 and not out.scalar else ''} {reg_out(out)}, {', '.join(reg_in(x) if x.__class__ is Register else str(x) for x in vin)}")
-      elif uop == UOps.LOAD:
+      elif uop == Ops.LOAD:
         if out.scalar:
           # swap arg order
           ins.append(f's_load_b32 {reg_out(out)}, {reg_in(vin[0])}, {reg_in(vin[1])} offset:{arg[0]}')
         else:
-          ins.append(f'global_load_{"b128" if out.dtype == dtypes._float4 else "b32"} {reg_out(out)}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
+          ins.append(f'global_load_{"b128" if out.dtype == dtypes.float.vec(4) else "b32"} {reg_out(out)}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
         pend_regs.add(out)
         for r in out.subregs(): pend_regs.add(r)
-      elif uop == UOps.STORE:
-        ins.append(f'global_store_{"b128" if vin[1].dtype == dtypes._float4 else "b32"} {reg_in(vin[2])}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
-      elif uop == UOps.LABEL:
+      elif uop == Ops.STORE:
+        ins.append(f'global_store_{"b128" if vin[1].dtype == dtypes.float.vec(4) else "b32"} {reg_in(vin[2])}, {reg_in(vin[1])}, {reg_in(vin[0])} offset:{arg[0]}')
+      elif uop == Ops.LABEL:
         ins.append(f"{arg}:")
-      elif uop == UOps.COND_BRANCH:
+      elif uop == Ops.COND_BRANCH:
         ins.append(f"s_cbranch_scc{'1' if arg[1] else '0'} {arg[0]}")
-      elif uop == UOps.CAST:
+      elif uop == Ops.CAST:
         if vin[0].dtype == dtypes.bool:
           if out.dtype == dtypes.float32:
             ins.append(f"v_cndmask_b32 {reg_out(out)}, 0.0, 1.0, {reg_in(vin[0])}")