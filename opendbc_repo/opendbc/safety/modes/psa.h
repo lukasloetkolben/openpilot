b245402f319115f4dfe3b2595e37776c221673fc
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2026-07-09 17:02:16 +0200

    PSA: safety with camera harness

--- opendbc_repo/opendbc/safety/modes/psa.h
+++ opendbc_repo/opendbc/safety/modes/psa.h
@@ -6,7 +6,6 @@
 #define PSA_STEERING_ALT          773U  // RX from EPS, steering angle
 #define PSA_DYN_CMM               520U  // RX from CMM, gas pedal
 #define PSA_HS2_DYN_ABR_38D       909U  // RX from UC_FREIN, speed
-#define PSA_HS2_DAT_MDD_CMD_452   1106U // RX from BSI, cruise state
 #define PSA_DAT_BSI               1042U // RX from BSI, brake
 #define PSA_LANE_KEEP_ASSIST      1010U // TX from OP,  EPS
 
@@ -17,9 +16,7 @@
 
 static uint8_t psa_get_counter(const CANPacket_t *msg) {
   uint8_t cnt = 0;
-  if (msg->addr == PSA_HS2_DAT_MDD_CMD_452) {
-    cnt = (msg->data[3] >> 4) & 0xFU;
-  } else if (msg->addr == PSA_HS2_DYN_ABR_38D) {
+  if (msg->addr == PSA_HS2_DYN_ABR_38D) {
     cnt = (msg->data[5] >> 4) & 0xFU;
   } else {
   }
@@ -48,9 +45,7 @@ static uint8_t _psa_compute_checksum(const CANPacket_t *msg, uint8_t chk_ini, in
 
 static uint32_t psa_compute_checksum(const CANPacket_t *msg) {
   uint8_t chk = 0;
-  if (msg->addr == PSA_HS2_DAT_MDD_CMD_452) {
-    chk = _psa_compute_checksum(msg, 0x4, 5);
-  } else if (msg->addr == PSA_HS2_DYN_ABR_38D) {
+  if (msg->addr == PSA_HS2_DYN_ABR_38D) {
     chk = _psa_compute_checksum(msg, 0x7, 5);
   } else {
   }
@@ -71,20 +66,12 @@ static void psa_rx_hook(const CANPacket_t *msg) {
       vehicle_moving = speed > 0;
       UPDATE_VEHICLE_SPEED(speed * 0.01 * KPH_TO_MS); // VITESSE_VEHICULE_ROUES
     }
-  }
-
-  if (msg->bus == PSA_ADAS_BUS) {
-    if (msg->addr == PSA_HS2_DAT_MDD_CMD_452) {
-      pcm_cruise_check((msg->data[2U] >> 7U) & 1U); // RVV_ACC_ACTIVATION_REQ
-    }
-  }
-
-
-  if (msg->bus == PSA_CAM_BUS) {
     if (msg->addr == PSA_DAT_BSI) {
       brake_pressed = (msg->data[0U] >> 5U) & 1U; // P013_MainBrake
     }
   }
+
+  // TODO: find cruise state message, HS2_DAT_MDD_CMD_452 is not present on this car
 }
 
 static bool psa_tx_hook(const CANPacket_t *msg) {
@@ -119,16 +106,16 @@ static bool psa_tx_hook(const CANPacket_t *msg) {
 static safety_config psa_init(uint16_t param) {
   SAFETY_UNUSED(param);
   static const CanMsg PSA_TX_MSGS[] = {
-    {PSA_LANE_KEEP_ASSIST, PSA_MAIN_BUS, 8, .check_relay = true}, // EPS steering
+    // check_relay=false: a car-side ECU sends 0x3F2 even with the relay open
+    {PSA_LANE_KEEP_ASSIST, PSA_MAIN_BUS, 8, .check_relay = false}, // EPS steering
   };
 
   static RxCheck psa_rx_checks[] = {
-    {.msg = {{PSA_HS2_DAT_MDD_CMD_452, PSA_ADAS_BUS, 6, 20U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},                        // cruise state
     {.msg = {{PSA_HS2_DYN_ABR_38D, PSA_MAIN_BUS, 8, 25U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},                            // speed
     {.msg = {{PSA_STEERING_ALT, PSA_MAIN_BUS, 7, 100U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}}, // steering angle
     {.msg = {{PSA_STEERING, PSA_MAIN_BUS, 7, 100U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},     // driver torque
     {.msg = {{PSA_DYN_CMM, PSA_MAIN_BUS, 8, 100U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},      // gas pedal
-    {.msg = {{PSA_DAT_BSI, PSA_CAM_BUS, 8, 20U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},        // brake
+    {.msg = {{PSA_DAT_BSI, PSA_MAIN_BUS, 8, 20U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},       // brake
   };
 
   return BUILD_SAFETY_CFG(psa_rx_checks, PSA_TX_MSGS);
